f39032b8fa6cf09ea0cbd72e7a200e4dc22cf942
exromany <exromany@gmail.com>
2024-02-01 07:54:47 +0300

    feat: deploy with base

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import svgrPlugin from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
-export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgrPlugin(), nodePolyfills()],
-  build: {
-    chunkSizeWarningLimit: 2000,
-  },
-  base: '/',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react(), tsconfigPaths(), svgrPlugin(), nodePolyfills()],
+    build: {
+      chunkSizeWarningLimit: 2000,
+    },
+    base: env.BASE,
+  };
 });
